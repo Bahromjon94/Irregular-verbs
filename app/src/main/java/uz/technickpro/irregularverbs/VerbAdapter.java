503c3b151342d4bbac4f032817db807b21d4503d
Bahromjon94 <bahromjon.ergashboyev@gmail.com>
2021-02-07 11:37:35 +0500

    Bookmarks iconkalar qo'shildi

--- app/src/main/java/uz/technickpro/irregularverbs/VerbAdapter.java
+++ app/src/main/java/uz/technickpro/irregularverbs/VerbAdapter.java
@@ -1,15 +1,21 @@
 package uz.technickpro.irregularverbs;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
 
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+
 public class VerbAdapter extends BaseAdapter {
+
+    private static final String TAG = "VerbAdapter";
 
 
     Context context;
@@ -44,11 +50,41 @@ public class VerbAdapter extends BaseAdapter {
         TextView verbOne = convertView.findViewById(R.id.item_verb_one_text);
         TextView verbTwo = convertView.findViewById(R.id.item_verb_two_text);
         TextView verbThree = convertView.findViewById(R.id.item_verb_three_text);
+        ImageView imgAddToBookmark = convertView.findViewById(R.id.item_verb_add_bookmark_img);
+        ImageView imgAddedToBookmark = convertView.findViewById(R.id.item_verb_added_bookmark_img);
 
         word.setText(verbs.get(position).getWord());
         verbOne.setText(verbs.get(position).getV1());
         verbTwo.setText(verbs.get(position).getV2());
         verbThree.setText(verbs.get(position).getV3());
+        imgAddToBookmark.setImageResource(R.drawable.ic_baseline_bookmark_border_24);
+        imgAddedToBookmark.setImageResource(R.drawable.ic_baseline_bookmark_24);
+
+
+        imgAddToBookmark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgAddToBookmark.setVisibility(View.GONE);
+                imgAddedToBookmark.setVisibility(View.VISIBLE);
+
+                Log.d(TAG, "onClick: " + verbs.get(position).getWord() + " was added");
+            }
+        });
+
+
+        imgAddedToBookmark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                imgAddedToBookmark.setVisibility(View.GONE);
+                imgAddToBookmark.setVisibility(View.VISIBLE);
+
+                Log.d(TAG, "onClick: " + verbs.get(position).getWord() + " was deleted");
+            }
+        });
+
+
+
 
         return convertView;
     }
